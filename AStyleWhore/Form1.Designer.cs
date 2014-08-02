0e253f14fc90b135bc8297a5a0fcd9ad2f3666c7
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-08-02 23:33:20 +0200

    center the form on start

--- AStyleWhore/Form1.Designer.cs
+++ AStyleWhore/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AStyleWhore";
             this.ResumeLayout(false);
 
