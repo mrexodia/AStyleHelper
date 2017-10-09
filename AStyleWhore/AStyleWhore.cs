7ebe74666c93742b74d18667ad7e61952f818a56
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2017-10-09 23:41:17 +0200

    use git ls-files to find which files to format

--- AStyleWhore/AStyleWhore.cs
+++ AStyleWhore/AStyleWhore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
 
 namespace AStyleWhore
 {
@@ -19,12 +20,34 @@ namespace AStyleWhore
             return retlist;
         }
 
+        //https://stackoverflow.com/a/206347
+        private static string[] GitLsFiles(string dir, string pattern)
+        {
+            try
+            {
+                var p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = "git";
+                p.StartInfo.Arguments = "ls-files " + pattern.Replace(';', ' ');
+                p.Start();
+                var output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                return output.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch
+            {
+                return GetFilesInDir(dir, pattern);
+            }
+        }
+
         public static string AStyleDirectory(string dir, ref bool changesMade, string pattern = "*.c;*.h;*.cpp;*.hpp", string options = "style=allman, convert-tabs, align-pointer=type, align-reference=middle, indent=spaces, indent-namespaces, indent-col1-comments, pad-oper, unpad-paren, keep-one-line-blocks, close-templates")
         {
             changesMade = false;
 
             // Get a list of source files
-            string[] sources = GetFilesInDir(dir, pattern);
+            string[] sources = GitLsFiles(dir, pattern);
 
             if (sources.Length <= 0)
                 return "No source files found!";
@@ -36,12 +59,9 @@ namespace AStyleWhore
 
             foreach (string file in sources)
             {
+                File.AppendAllText("formatted.txt", file + "\n");
                 try
                 {
-                    // Skip Qt build files
-                    if (file.Contains("bin\\") || file.Contains("release\\") || file.Contains("gui_build\\"))
-                        continue;
-
                     string fileText = File.ReadAllText(file);
                     string formatText = AStyle.FormatSource(fileText, options).Replace("\r\n", "\n").Replace("\n", "\r\n");
 
